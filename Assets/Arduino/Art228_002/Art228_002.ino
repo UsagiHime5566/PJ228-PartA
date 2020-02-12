ca3b16a74b09f572ba169be831a50175feeda013
nikki <cuapopomama@gmail.com>
2020-02-12 10:17:37 +0800

    Update Art228_002.ino

--- Assets/Arduino/Art228_002/Art228_002.ino
+++ Assets/Arduino/Art228_002/Art228_002.ino
@@ -1,43 +1,34 @@
 int ledPin = 13; // LED connected to digital pin 13
 int inPin = 7;   // pushbutton connected to digital pin 7
 int val = 0;     // variable to store the read value
-int tamp = 10;
+boolean hasUserIn;
 
 void setup()
 {
   Serial.begin(9600);
   pinMode(ledPin, OUTPUT);      // sets the digital pin 13 as output
   pinMode(inPin, INPUT);      // sets the digital pin 7 as input
+  hasUserIn = false;
 }
 
 void loop()
 {
   val = digitalRead(inPin);   // read the input pin
   
-  if(val==0)
-  {
-    if(tamp!=0)
-    {
-    Serial.write(0);
+  if(val==0) {  //if no user
+    //Serial.write(0);
     //Serial.println(0, DEC);
     digitalWrite(ledPin, LOW);
-    tamp=0;
-    }  
+    hasUserIn = false;
   }
- 
-  if(val==1)
-  {
-    if(tamp!=1)
-    {
-    Serial.write(1);
-    //Serial.println(1, DEC);
-    digitalWrite(ledPin, HIGH);
-    tamp=1;
-    }  
+  if(val==1) {  // if user go in
+    if(hasUserIn == false) {
+      //Serial.write(1);
+      //Serial.println(1, DEC);
+      Serial.println("1");
+      digitalWrite(ledPin, HIGH);
+      hasUserIn = true;
+    }
   }
-
   delay(50);
-
-
-  
 }
