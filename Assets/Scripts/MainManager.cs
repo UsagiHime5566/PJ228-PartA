9e1aa8e3507a274ef0638fa88a89ca7a12d0b913
nikki <cuapopomama@gmail.com>
2020-02-13 00:07:29 +0800

    滑鼠隱藏功能

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -14,6 +14,10 @@ public class MainManager : MonoBehaviour
 
     int localPlayTime = 0;
 
+    //Cursor
+    int cursorStayTime = 0;
+    Vector3 lastCursorPos;
+
     void Start()
     {
         SignalAdapter.OnRecieveRun += RecieveRunSignal;
@@ -25,6 +29,20 @@ public class MainManager : MonoBehaviour
 
         RunBatCmd.CreateBatFile();
         StartCoroutine(DelayCloseOption());
+    }
+
+    void Update(){
+        if(lastCursorPos != Input.mousePosition){
+            Cursor.visible = true;
+            cursorStayTime = 0;
+        } else {
+            if(cursorStayTime < 30){
+                cursorStayTime++;
+            } else {
+                Cursor.visible = false;
+            }
+        }
+        lastCursorPos = Input.mousePosition;
     }
 
     IEnumerator DelayCloseOption()
